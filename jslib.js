1843a6b5ed82c1e0b9698334af038407cc780bac
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-09-16 18:13:48 +0200

    [1.2.1] removed notification API (malfunction)

--- jslib.js
+++ jslib.js
@@ -1,13 +1,12 @@
 /*
 
-JSLib v1.2.0 - JavaScript library that is used by most of Sv443's JS projects that makes coding a bit faster and takes away a bit of the pain in the butt
+JSLib v1.2.1 - JavaScript library that is used by most of Sv443's JS projects that makes coding a bit faster and takes away a bit of the pain in the butt
 For full documentation please visit https://github.com/Sv443/jslib
 To report bugs, suggest features or send in code, please go to https://github.com/Sv443/jslib/issues
 
 
-v1.2.0 changelog:
-	- added Element.new(), Element.remove(), Element.removeOther(), Element.setAttribute(), Element.innerhtml() and Element.outerhtml() to add, remove and modify elements easily
-	- added EventListener to close all opened menus if ESC is pressed
+v1.2.1 changelog:
+	- removed notification API due to malfunction
 */
 
 
@@ -23,6 +22,9 @@ var errortitle = ["Oh no! ", "Whoops! ", "Allan, please fix this.\n", ":(\n", "R
 var Menu = new Menu(), Notif = new Notification(), Audio = new Audio(), Element = new Element();
 console.log("Powered by " + jslname + " v" + jslversion + " - (c) Sv443 / Sven Fehler 2018 ( https://www.github.com/Sv443 ) - licensed under the MIT license");
 
+
+!function(a){var b=/iPhone/i,c=/iPod/i,d=/iPad/i,e=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,f=/Android/i,g=/(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i,h=/(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i,i=/IEMobile/i,j=/(?=.*\bWindows\b)(?=.*\bARM\b)/i,k=/BlackBerry/i,l=/BB10/i,m=/Opera Mini/i,n=/(CriOS|Chrome)(?=.*\bMobile\b)/i,o=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,p=new RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),q=function(a,b){return a.test(b)},r=function(a){var r=a||navigator.userAgent,s=r.split("[FBAN");return"undefined"!=typeof s[1]&&(r=s[0]),s=r.split("Twitter"),"undefined"!=typeof s[1]&&(r=s[0]),this.apple={phone:q(b,r),ipod:q(c,r),tablet:!q(b,r)&&q(d,r),device:q(b,r)||q(c,r)||q(d,r)},this.amazon={phone:q(g,r),tablet:!q(g,r)&&q(h,r),device:q(g,r)||q(h,r)},this.android={phone:q(g,r)||q(e,r),tablet:!q(g,r)&&!q(e,r)&&(q(h,r)||q(f,r)),device:q(g,r)||q(h,r)||q(e,r)||q(f,r)},this.windows={phone:q(i,r),tablet:q(j,r),device:q(i,r)||q(j,r)},this.other={blackberry:q(k,r),blackberry10:q(l,r),opera:q(m,r),firefox:q(o,r),chrome:q(n,r),device:q(k,r)||q(l,r)||q(m,r)||q(o,r)||q(n,r)},this.seven_inch=q(p,r),this.any=this.apple.device||this.android.device||this.windows.device||this.other.device||this.seven_inch,this.phone=this.apple.phone||this.android.phone||this.windows.phone,this.tablet=this.apple.tablet||this.android.tablet||this.windows.tablet,"undefined"==typeof window?this:void 0},s=function(){var a=new r;return a.Class=r,a};"undefined"!=typeof module&&module.exports&&"undefined"==typeof window?module.exports=r:"undefined"!=typeof module&&module.exports&&"undefined"!=typeof window?module.exports=s():"function"==typeof define&&define.amd?define("isMobile",[],a.isMobile=s()):a.isMobile=s()}(this);
+var ismobile;if(isMobile.any) ismobile = true;else ismobile = false;
 
 
 
@@ -195,28 +197,6 @@ function Audio(){
 	}
 }
 
-// to enable notifications, your manifest.json file must contain the following, else it won't work:         "permissions":{"desktop-notification":{}}
-// more info over at https://developer.mozilla.org/en-US/docs/Web/API/notification
-// use description to add a description and iconsrc to add an icon URL - both are optional and can be left empty, null or undefined to disable
-function Notification(){
-	this.send = function(title, description, iconsrc){
-		if(isempty(description)) description = "";
-		if(isempty(iconsrc)) iconsrc = "";
-		if(!("Notification" in window)) {
-			clog("This browser does not support desktop notifications");
-		}
-		else if (Notification.permission === "granted") {
-			var notification = new Notification(title, {body: description, icon: iconsrc});
-		}
-		else if (Notification.permission !== 'denied') {
-			Notification.requestPermission(function (permission) {
-				if (permission === "granted") {
-					var notification = new Notification(title, {body: description, icon: iconsrc});
-				}
-			});
-		}
-	}
-}
 
 // use id "demo" to see an example
 // type is the HTML tag name (for example "div", "a", or "iframe")
